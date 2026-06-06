ee3457437d6998e6a5eaeb63f8465a524ee70979
shrihari-kallapur <shriharikallapur7@gmail.com>
2026-06-06 19:44:14 +0530

    fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from dotenv import load_dotenv
+
 from app.routes.portfolio import router as portfolio_router
 from app.routes.zerodha import router as zerodha_router
-
-from dotenv import load_dotenv
 
 load_dotenv()
 
 app = FastAPI()
+
+# CORS MUST COME BEFORE ROUTES
 
 app.add_middleware(
 
@@ -23,7 +25,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Routes
+
 app.include_router(zerodha_router)
+
+app.include_router(portfolio_router)
 
 @app.get("/")
 def home():
@@ -31,5 +37,3 @@ def home():
     return {
         "message": "Finance Dashboard API Running"
     }
-
-app.include_router(portfolio_router)
